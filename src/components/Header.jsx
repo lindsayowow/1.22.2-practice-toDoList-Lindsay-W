80a5e22c871d6b7bba82fd792bbbc99d5d8b6ee2
lindsayowow <lindsayow@gmail.com>
2026-02-09 20:47:05 -0600

    uploading final updates

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+// ○ Create a functional component called Header. 
+// ○ Inside this component: 
+// ■ Render a simple heading that says "My To-Do List". 
+
+// import './App.css';
+
+function Header() {
+    return (
+        <>
+        <h1>My To-Do List</h1>
+        </>
+    );
+}
+
+export default Header;
---
+++ src/components/ToDoList.jsx
@@ -0,0 +1,21 @@
+// ○ Create a functional component called ToDoList. 
+// ○ Inside this component: 
+// ■ Hard code a To-Do list to display a few tasks as a bulleted list 
+// (e.g., "Learn React", "Build a project", "Read 
+// documentation"). 
+
+// import './App.css';
+
+function ToDoList () {
+    return (
+        <>
+       <ul>
+        <li>Learn React</li>
+        <li>Build a project</li>
+        <li>Read documentation</li>
+       </ul>
+        </>
+    );
+}
+
+export default ToDoList;
